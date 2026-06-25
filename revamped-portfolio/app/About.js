f1617f7e90441b0bbddd46cf1514e37e1ee353a7
sagesaintsalem <ms.r.toussaint@gmail.com>
2026-06-26 00:09:33 +0100

    Padding tweaks in About

--- revamped-portfolio/app/About.js
+++ revamped-portfolio/app/About.js
@@ -9,9 +9,9 @@ const About = () => {
                             I have a flair for front-end but I also have skills to share in back-end development too! I spend a lot of my time sharpening my skills, attending networking events and workshops, volunteering, keeping my LinkedIn page up to date and building my portfolio site (psst - you&apos;re already here!). </p><p className="bg-pinky-purple p-6 lg:text-2xl md:text-xl sm:text-lg w-fit mb-4">I am currently looking for work!</p>
             
 
-                        <div className="bg-green-500 dark:bg-green-300 text-black p-4 rounded-2xl ">
+                        <div className="bg-green-500/30 dark:bg-green-300/40 text-black p-6 rounded-2xl ">
                         <p className='font-bold border-b border-black text-xl p-2'>Stack</p>
-                            <ul className="grid grid-cols-2 grid-rows-8 justify-around gap-3 p-2">
+                            <ul className="grid grid-cols-2 grid-rows-8 justify-around gap-3 p-6">
                             <li >Javascript</li>
                             <li >Python</li>
                             <li >Java</li>
@@ -29,7 +29,7 @@ const About = () => {
                             </ul>
 
                             <p className='font-bold border-t border-b border-black text-xl p-2'>Skills</p>
-                            <ul className="grid grid-cols-2 grid-rows-4 justify-around gap-3 p-2">
+                            <ul className="grid grid-cols-2 grid-rows-4 justify-around gap-3 p-6">
                                 <li >French</li>
                                 <li >Teaching</li>
                                 <li >App Publishing</li>
